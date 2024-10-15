de995b7350bea132f234b8f46c32755bfb975718
amelie <ameliie.rikaud@gmail.com>
2024-10-15 16:40:30 +0200

    Problème DAO

--- back-skeleton/src/main/java/com/takima/backskeleton/dao/LivreDao.java
+++ back-skeleton/src/main/java/com/takima/backskeleton/dao/LivreDao.java
@@ -1,27 +1,4 @@
-package com.takima.backskeleton.dao;
+package com.takima.backskeleton.DAO;
 
-import com.takima.backskeleton.models.Livres;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
-@Repository
-public interface LivreDao extends JpaRepository<Livres, Long> {
-
-    List<Livres> findByTitre(String titre);
-    List<Livres> findByAuteur(String auteur);
-    List<Livres> findByEdition(String edition);
-    List<Livres> findAll();
-
-    Livres findByIdLivre(long id);
-
-    default Livres creerLivre(Livres livre) {
-        return save(livre);
-    }
-
-//     Méthode pour supprimer un livre par ID
-    default void supprimerLivre(Long idLivre) {
-        deleteById(idLivre);
-    }
+public interface LivreDao {
 }
